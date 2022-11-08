9129f7dd62866d70224e0a35814473edbb053277
Murilo Pinheiro <pinheiromurilo13@gmail.com>
2022-11-07 22:08:59 -0300

    finishing SSR Authentication

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,3 +1,4 @@
+import { destroyCookie } from "nookies"
 import { useContext, useEffect } from "react"
 import { AuthContext } from "../contexts/AuthContext"
 import { setupAPIClient } from "../services/api"
@@ -20,8 +21,7 @@ export default function Dashboard(){
 export const getServerSideProps = withSSRAuth( async (ctx) => {
     const apiClient = setupAPIClient(ctx);
     const response = await apiClient.get('/me');
-
-    console.log(response.data)
+    
 
     return {
         props: {}
--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError } from 'axios';
 import { parseCookies, setCookie } from 'nookies'
 import { signOut } from '../contexts/AuthContext';
+import { AuthTokenError } from './errors/AuthTokenError';
 
 interface AxiosErrorResponse {
     code?: string;
@@ -57,7 +58,7 @@ export function setupAPIClient(ctx = undefined) {
                         failedRequestsQueue.forEach(request => request.onFailure(err))
                         failedRequestsQueue = []
 
-                        if (process.browser){
+                        if (typeof window === "undefined"){
                             signOut()
                         }
                         
@@ -80,8 +81,10 @@ export function setupAPIClient(ctx = undefined) {
                 })
     
                 } else {
-                    if (process.browser){
+                    if (typeof window === "undefined"){
                         signOut()
+                    } else {
+                        return Promise.reject(new AuthTokenError())
                     }
     
                 }
